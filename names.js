8cc74b111aec37bf00cd389acdbda7491c11c940
Jerika Kanz <jerikacapila@gmail.com>
2022-10-11 12:12:33 +0200

    Exercise 2

---
+++ names.js
@@ -0,0 +1,9 @@
+const products = ["Mango", "Orange Juice", "Soy Sauce", "Rice", "Tissue", 
+"Chicken"]
+
+console.log(`
+Your Grocery List:
+Count of groceries:  ${products.length}
+`)
+
+products.forEach(product =>  console.log(product))
